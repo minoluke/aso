c2edc9510e98d39678e874c2d5a93473a96bd32a
minoluke <ideno.minoru@gmail.com>
2024-07-07 16:52:32 +0900

    sdsd

--- scripts/run_forecast.sh
+++ scripts/run_forecast.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
 # 定義された値の配列
-window_lengths=(30 60 90 120)
-look_forwards=(30 60 90 120)
-
+window_lengths=(10 30 50 70 90 110 130)
+look_forwards=(10 30 50 70 90 110 130)
+cvs=(0 1 2 3 4 5)
 
 # それぞれの組み合わせで実行
 for window_length in "${window_lengths[@]}"
-do
-    for look_forward in "${look_forwards[@]}"
-    do
-        combination="${window_length}_${look_forward}"
-        if [[ " ${skip_combinations[@]} " =~ " ${combination} " ]]; then
-            echo "Skipping window_length=${window_length} and look_forward=${look_forward}"
-        else
-            echo "Running with window_length=${window_length} and look_forward=${look_forward}"
-            python forecast_model.py $window_length $look_forward
-        fi
+dorforecast_model.py tilt $window_length $look_forward $cv
+        done
     done
 done
