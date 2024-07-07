8620d4fa88bde0722530718f46ab374cb27b2ab8
minoluke <ideno.minoru@gmail.com>
2024-07-07 17:12:57 +0900

    dfd

--- scripts/run_forecast.sh
+++ scripts/run_forecast.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# コマンドライン引数を変数に代入
+mode=$1
+
 # 定義された値の配列
-window_lengths=(10 30 50 70 90 110 130)
-look_forwards=(10 30 50 70 90 110 130)
+window_lengths=(10 30 60 90 120)
+look_forwards=(10 30 60 90 120)
 cvs=(0 1 2 3 4 5)
 
 # それぞれの組み合わせで実行
@@ -12,8 +15,8 @@ do
     do
         for cv in "${cvs[@]}"
         do
-            echo "Running with window_length=${window_length}, look_forward=${look_forward}, cv=${cv}"
-            python forecast_model.py tilt $window_length $look_forward $cv
+            echo "Running with mode=${mode}, window_length=${window_length}, look_forward=${look_forward}, cv=${cv}"
+            python forecast_model.py $mode $window_length $look_forward $cv
         done
     done
 done
