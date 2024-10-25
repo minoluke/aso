21255fcc1139268bacd8a02e034707923502478a
minoluke <ideno.minoru@gmail.com>
2024-10-25 18:37:17 +0900

    tat

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ def train_test(od,lb,lf,cv):
 
     month = timedelta(days=365.25/12)
     observation_m = ObservationData()
-
-    observation_start = time.time()
     te = observation_m.tes[int(cv)]
-    print(f"Observation data loading time: {time.time() - observation_start:.2f} seconds")
 
     n_jobs = 6
         
@@ -37,15 +34,17 @@ def train_test(od,lb,lf,cv):
     if data_streams is None:
         raise ValueError("Invalid value for 'od'")
 
-    train_start = time.time()
+    
     train_m = TrainModel(ti='2010-01-01', tf='2022-12-31', look_backward=float(lb), overlap=0.85, look_forward=float(lf), data_streams=data_streams, od=od)
     train_m.train(cv=cv, ti='2010-01-01', tf='2022-12-31', retrain=True, exclude_dates=[[te-6*month,te+6*month],], n_jobs=n_jobs) 
-    print(f"Training time: {time.time() - train_start:.2f} seconds")
 
     test_start = time.time()
     test_m = TestModel(ti='2010-01-01', tf='2022-12-31', look_backward=float(lb), overlap=0.85, look_forward=float(lf), data_streams=data_streams, od=od)
     test_m.test(cv=cv, ti='2010-01-01', tf='2022-12-31', recalculate=True, n_jobs=n_jobs)  
     print(f"Testing time: {time.time() - test_start:.2f} seconds")
+
+    total_time = time.time() - start_time
+    print(f"Total execution time: {total_time:.2f} seconds")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
