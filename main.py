fe794d26a248571e33069ff4d00d462002120d2c
minoluke <ideno.minoru@gmail.com>
2024-10-25 21:42:50 +0900

    main

--- main.py
+++ main.py
@@ -9,18 +9,7 @@ sys.path.insert(0, os.path.abspath('..'))
 from modules import *
 logging.getLogger("tsfresh").setLevel(logging.ERROR)
 
-import time 
-
-def train_test(od,lb,lf,cv):
-    start_time = time.time()
-
-    month = timedelta(days=365.25/12)
-    observation_m = ObservationData()
-    te = observation_m.tes[int(cv)]
-
-    n_jobs = 6
-        
-    data_streams_dict = {
+data_streams_dict = {
         'tremor': ['rsam', 'mf', 'hf', 'dsar'],
         'gas': ['gas_max', 'gas_min', 'gas_mean', 'gas_number'],
         'magnetic': ['magnetic'],
@@ -30,21 +19,27 @@ def train_test(od,lb,lf,cv):
         'all': ['rsam', 'mf', 'hf', 'dsar', 'gas_max', 'gas_min', 'gas_mean', 'gas_number','magnetic', 'kakouwall_temp', 'tilt1_NS', 'tilt1_EW', 'tilt2_NS', 'tilt2_EW','yudamari_number', 'yudamari_temp']
     }
 
+month = timedelta(days=365.25/12)
+n_jobs = 6
+observation_m = ObservationData()
+
+start_period = '2010-01-01'
+end_period = '2022-12-31'
+
+overlap = 0.85
+
+def one_train_test(od,lb,lf,cv):
+    te = observation_m.tes[int(cv)]
+
     data_streams = data_streams_dict.get(od)
     if data_streams is None:
         raise ValueError("Invalid value for 'od'")
-
     
-    train_m = TrainModel(ti='2010-01-01', tf='2022-12-31', look_backward=float(lb), overlap=0.85, look_forward=float(lf), data_streams=data_streams, od=od)
-    train_m.train(cv=cv, ti='2010-01-01', tf='2022-12-31', retrain=True, exclude_dates=[[te-6*month,te+6*month],], n_jobs=n_jobs) 
+    train_m = TrainModel(ti=start_period, tf=end_period, look_backward=float(lb), overlap=overlap, look_forward=float(lf), data_streams=data_streams, od=od)
+    train_m.train(cv=cv, ti=start_period, tf=end_period, retrain=True, exclude_dates=[[te-6*month,te+6*month],], n_jobs=n_jobs) 
 
-    test_start = time.time()
-    test_m = TestModel(ti='2010-01-01', tf='2022-12-31', look_backward=float(lb), overlap=0.85, look_forward=float(lf), data_streams=data_streams, od=od)
-    test_m.test(cv=cv, ti='2010-01-01', tf='2022-12-31', recalculate=True, n_jobs=n_jobs)  
-    print(f"Testing time: {time.time() - test_start:.2f} seconds")
-
-    total_time = time.time() - start_time
-    print(f"Total execution time: {total_time:.2f} seconds")
+    test_m = TestModel(ti=start_period, tf=end_period, look_backward=float(lb), overlap=overlap, look_forward=float(lf), data_streams=data_streams, od=od)
+    test_m.test(cv=cv, ti=start_period, tf=end_period, recalculate=True, n_jobs=n_jobs)  
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -56,5 +51,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     
-    train_test(args.od,args.lb, args.lf,args.cv)
+    one_train_test(args.od,args.lb, args.lf,args.cv)
     
